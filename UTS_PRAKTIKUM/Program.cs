ca76ad0ef048a3aeeb46ce1c91282563fbf22db2
Lenovo <Lenovo@LENOVO-PC>
2025-04-25 19:46:04 +0700

    Add project files.

---
+++ UTS_PRAKTIKUM/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+class program 
+{
+    static void Main()
+    {
+        Console.WriteLine($"1. Tambah Mahasiswa");
+        Console.WriteLine($"2. Lihat Mahasiswa");
+        Console.WriteLine($"3. Update Mahasiswa");
+        Console.WriteLine($"4. Hapus Mahasiswa");
+        Console.WriteLine($"5. Keluar");
+       
+    }
+        
+}
+
+class Mahasiswa
+{
+    public int Nim;
+    public string Nama;
+    public string Jurusan;
+}
+
+class Nama
+{
+    
+
+}
+
+class Jurusan
+{
+    
+
+}
